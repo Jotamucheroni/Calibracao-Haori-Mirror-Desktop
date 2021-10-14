57397d21490e7274a8287dbc80d694a479821ad1
João Pedro Mucheroni Covolan <jotamucheroni@gmail.com>
2021-10-14 19:45:41 -0300

    Dispositivo - calibração projeção

--- src/aplicativo/es/dispositivo/Dispositivo.java
+++ src/aplicativo/es/dispositivo/Dispositivo.java
@@ -2,6 +2,7 @@ package aplicativo.es.dispositivo;
 
 import java.util.List;
 
+import aplicativo.Aplicativo;
 import aplicativo.es.camera.Camera;
 import aplicativo.opengl.Programa;
 import aplicativo.opengl.Desenho;
@@ -11,6 +12,7 @@ import aplicativo.otimizacao.EnxameParticulas;
 import aplicativo.otimizacao.EstrategiaEvolutiva;
 import aplicativo.otimizacao.FuncaoParametrosExtrinsecos;
 import aplicativo.otimizacao.FuncaoParametrosIntrinsecos;
+import aplicativo.otimizacao.FuncaoParametrosProjecao;
 import aplicativo.otimizacao.RecozimentoSimulado;
 import aplicativo.pontos.DetectorPontos;
 import aplicativo.pontos.Marcador;
@@ -407,7 +409,7 @@ public class Dispositivo implements AutoCloseable {
         translacaoXOtimo, translacaoYOtimo, translacaoZOtimo,
         rotacaoXOtimo, rotacaoYOtimo, rotacaoZOtimo,
         aptidaoExtrinsecosOtima = Float.MAX_VALUE;
-    private final float anguloRadiano = (float) Math.toRadians( 5 );
+    private final float anguloRadianoExtrinsecos = (float) Math.toRadians( 5 );
     
     public void calibrarParametrosExtrinsecos( Dispositivo dispositivoReferencia ) {
         if (
@@ -494,15 +496,15 @@ public class Dispositivo implements AutoCloseable {
                     minimo[0] = xMedio - xMedio * 0.5f;
                     minimo[1] = yMedio - yMedio * 0.5f;
                     minimo[2] = zMedio - zMedio * 0.5f;
-                    minimo[3] = -anguloRadiano;
-                    minimo[4] = -anguloRadiano;
-                    minimo[5] = -anguloRadiano;
+                    minimo[3] = -anguloRadianoExtrinsecos;
+                    minimo[4] = -anguloRadianoExtrinsecos;
+                    minimo[5] = -anguloRadianoExtrinsecos;
                     maximo[0] = xMedio + xMedio * 0.5f;
                     maximo[1] = yMedio + yMedio * 0.5f;
                     maximo[2] = zMedio + zMedio * 0.5f;
-                    maximo[3] = +anguloRadiano;
-                    maximo[4] = +anguloRadiano;
-                    maximo[5] = +anguloRadiano;
+                    maximo[3] = +anguloRadianoExtrinsecos;
+                    maximo[4] = +anguloRadianoExtrinsecos;
+                    maximo[5] = +anguloRadianoExtrinsecos;
                     
                     funcao.setMarcador( marcador );
                     funcao.setMarcadorReferencia( marcadorReferencia );
@@ -664,11 +666,305 @@ public class Dispositivo implements AutoCloseable {
         return marcador;
     }
     
+    private Thread calibracaoParametrosProjecao;
+    private final Object travaParametrosProjecao = new Object();
+    private float
+        escalaProjecaoX, escalaProjecaoY,
+        rotacaoTelaX, rotacaoTelaY, rotacaoTelaZ,
+        translacaoTelaX, translacaoTelaY,
+        aptidaoProjecaoOtima = Float.MAX_VALUE;
+    private final float anguloRadianoProjecao = (float) Math.toRadians( 10 );
+    
+    public void calibrarParametrosProjecao() {
+        if ( calibracaoParametrosProjecao != null && calibracaoParametrosProjecao.isAlive() )
+            return;
+        
+        calibracaoParametrosProjecao = new Thread(
+            () ->
+            {
+                final FuncaoParametrosProjecao funcao = new FuncaoParametrosProjecao();
+                final int
+                    linhas = 4,
+                    colunas = 6; 
+                
+                RecozimentoSimulado recozimentoSimulado = new RecozimentoSimulado( funcao );
+                EstrategiaEvolutiva estrategiaEvolutiva = new EstrategiaEvolutiva( funcao );
+                EnxameParticulas enxameParticulas = new EnxameParticulas( funcao );
+                float[] resultadoRecozimento, resultadoEstrategia, resultadoEnxame;
+                float aptidaoRecozimento, aptidaoEstrategia, aptidaoEnxame;
+                
+                List<PontoMarcador> listaPontos;
+                Marcador marcador;
+                
+                Ponto3D
+                    superiorEsquerdo3D, superiorDireito3D, inferiorDireito3D, inferiorEsquerdo3D,
+                    pontoMundo;
+                Ponto2D
+                    superiorEsquerdo2D = new Ponto2D(),
+                    superiorDireito2D = new Ponto2D(),
+                    inferiorDireito2D = new Ponto2D(),
+                    inferiorEsquerdo2D = new Ponto2D(),
+                    pontoTela;
+                float
+                    ladoQuadrado,
+                    superiorEsquerdoX, superiorEsquerdoY,
+                    z,
+                    escalaXMedia, escalaYMedia,
+                    translacaoXMedia, translacaoYMedia;
+                float[]
+                    minimo = new float[7],
+                    maximo = new float[7];
+                
+                synchronized ( travaParametrosProjecao ) {
+                    aptidaoProjecaoOtima = Float.MAX_VALUE;
+                }
+                while( !Thread.currentThread().isInterrupted() ) {
+                    listaPontos =
+                        detectorPontos.getListaPontosMarcador();
+                    
+                    if ( listaPontos.size() < 4 )
+                        synchronized ( travaDetector ) {
+                            try {
+                                travaDetector.wait();
+                                continue;
+                            } catch ( InterruptedException e ) {
+                                return;
+                            }
+                        }
+                    
+                    marcador = new Marcador( linhas, colunas, listaPontos );
+                    
+                    int[][] quadrado = new int[][] {
+                        { 0, 3 },
+                        { 0, 2 },
+                        { 1, 3 },
+                        { 1, 2 },
+                        { 2, 3 },
+                        { 2, 2 }
+                    };
+                    int[] cantoQuadrado = quadrado[(int) Aplicativo.PARAMETROS[3].getValor( 6 )];
+                    
+                    superiorEsquerdo3D =
+                        marcador.getPontoGrade( cantoQuadrado[0], cantoQuadrado[1] ).getPontoMundo();
+                    superiorDireito3D =
+                        marcador.getPontoGrade( cantoQuadrado[0], cantoQuadrado[1] - 1 ).getPontoMundo();
+                    inferiorDireito3D =
+                        marcador.getPontoGrade( cantoQuadrado[0] + 1, cantoQuadrado[1] - 1 ).getPontoMundo();
+                    inferiorEsquerdo3D =
+                        marcador.getPontoGrade( cantoQuadrado[0] + 1, cantoQuadrado[1] ).getPontoMundo();
+                    
+                    /* superiorEsquerdo3D = marcador.getPontoGrade( 1, 3 ).getPontoMundo();
+                    superiorDireito3D = marcador.getPontoGrade( 1, 2 ).getPontoMundo();
+                    inferiorDireito3D = marcador.getPontoGrade( 2, 2 ).getPontoMundo();
+                    inferiorEsquerdo3D = marcador.getPontoGrade( 2, 3 ).getPontoMundo(); */
+                    
+                    superiorEsquerdoX = Aplicativo.PARAMETROS[3].getValor( 0 ) - 1;
+                    superiorEsquerdoY = Aplicativo.PARAMETROS[3].getValor( 1 ) - 1;
+                    ladoQuadrado = Aplicativo.PARAMETROS[3].getValor( 2 ) * 2;
+                    
+                    superiorEsquerdo2D.setCoordenadas(
+                        superiorEsquerdoX, superiorEsquerdoY
+                    );
+                    superiorDireito2D.setCoordenadas(
+                        superiorEsquerdoX + ladoQuadrado, superiorEsquerdoY
+                    );
+                    inferiorDireito2D.setCoordenadas(
+                        superiorEsquerdoX + ladoQuadrado, superiorEsquerdoY - ladoQuadrado
+                    );
+                    inferiorEsquerdo2D.setCoordenadas(
+                        superiorEsquerdoX, superiorEsquerdoY - ladoQuadrado
+                    );
+                    
+                    escalaXMedia = 0;
+                    escalaYMedia = 0;
+                    
+                    z = superiorEsquerdo3D.getZ();
+                    
+                    escalaXMedia +=
+                        ( superiorDireito2D.getX() - superiorEsquerdo2D.getX() ) * z /
+                        ( superiorDireito3D.getX() - superiorEsquerdo3D.getX() );
+                    escalaXMedia +=
+                        ( inferiorDireito2D.getX() - inferiorEsquerdo2D.getX() ) * z /
+                        ( inferiorDireito3D.getX() - inferiorEsquerdo3D.getX() );
+                    escalaXMedia /= 2;
+                    
+                    escalaYMedia +=
+                        ( superiorEsquerdo2D.getY() - inferiorEsquerdo2D.getY() ) * z /
+                        ( superiorEsquerdo3D.getY() - inferiorEsquerdo3D.getY() );
+                    escalaYMedia +=
+                        ( superiorDireito2D.getY() - inferiorDireito2D.getY() ) * z /
+                        ( superiorDireito3D.getY() - inferiorDireito3D.getY() );
+                    escalaYMedia /= 2;
+                    
+                    Ponto3D[] ponto3D = {
+                        superiorEsquerdo3D, superiorDireito3D, inferiorDireito3D, inferiorEsquerdo3D
+                    };
+                    Ponto2D[] ponto2D = {
+                        superiorEsquerdo2D, superiorDireito2D, inferiorDireito2D, inferiorEsquerdo2D
+                    };
+                    
+                    translacaoXMedia = 0;
+                    translacaoYMedia = 0;
+                    
+                    for ( int i = 0; i < ponto3D.length; i++ ) {
+                        pontoMundo = ponto3D[i];
+                        pontoTela = ponto2D[i];
+                        
+                        translacaoXMedia += pontoTela.getX() - pontoMundo.getX() * escalaXMedia / z;
+                        translacaoYMedia += pontoTela.getY() - pontoMundo.getY() * escalaYMedia / z;
+                    }
+                    
+                    translacaoXMedia /= 4;
+                    translacaoYMedia /= 4;
+                    
+                    minimo[0] = escalaXMedia - escalaXMedia * 0.5f;
+                    minimo[1] = escalaYMedia - escalaYMedia * 0.5f;
+                    minimo[2] = -anguloRadianoProjecao;
+                    minimo[3] = -anguloRadianoProjecao;
+                    minimo[4] = -anguloRadianoProjecao;
+                    minimo[5] = translacaoXMedia - translacaoXMedia * 0.5f;
+                    minimo[6] = translacaoYMedia - translacaoYMedia * 0.5f;
+                    
+                    maximo[0] = escalaXMedia + escalaXMedia * 0.5f;
+                    maximo[1] = escalaYMedia + escalaYMedia * 0.5f;
+                    maximo[2] = +anguloRadianoProjecao;
+                    maximo[3] = +anguloRadianoProjecao;
+                    maximo[4] = +anguloRadianoProjecao;
+                    maximo[5] = translacaoXMedia + translacaoXMedia * 0.5f;
+                    maximo[6] = translacaoYMedia + translacaoYMedia * 0.5f;
+                    
+                    funcao.setPontos3D(
+                        superiorEsquerdo3D, superiorDireito3D, inferiorDireito3D, inferiorEsquerdo3D
+                    );
+                    funcao.setPontos2D(
+                        superiorEsquerdo2D, superiorDireito2D, inferiorDireito2D, inferiorEsquerdo2D
+                    );
+                    
+                    resultadoRecozimento = recozimentoSimulado.otimizar( minimo, maximo );
+                    resultadoEstrategia = estrategiaEvolutiva.otimizar( minimo, maximo );
+                    resultadoEnxame = enxameParticulas.otimizar( minimo, maximo );
+                    
+                    aptidaoRecozimento = funcao.f( resultadoRecozimento );
+                    aptidaoEstrategia = funcao.f( resultadoEstrategia );
+                    aptidaoEnxame = funcao.f( resultadoEnxame );
+                    
+                    synchronized ( travaParametrosProjecao ) {
+                        if ( aptidaoRecozimento < aptidaoProjecaoOtima ) {
+                            aptidaoProjecaoOtima = aptidaoRecozimento;
+                            escalaProjecaoX = resultadoRecozimento[0];
+                            escalaProjecaoY = resultadoRecozimento[1];
+                            rotacaoTelaX = resultadoRecozimento[2];
+                            rotacaoTelaY = resultadoRecozimento[3];
+                            rotacaoTelaZ = resultadoRecozimento[4];
+                            translacaoTelaX = resultadoRecozimento[5];
+                            translacaoTelaY = resultadoRecozimento[6];
+                        }
+                        
+                        if ( aptidaoEstrategia < aptidaoProjecaoOtima ) {
+                            aptidaoProjecaoOtima = aptidaoEstrategia;
+                            escalaProjecaoX = resultadoEstrategia[0];
+                            escalaProjecaoY = resultadoEstrategia[1];
+                            rotacaoTelaX = resultadoEstrategia[2];
+                            rotacaoTelaY = resultadoEstrategia[3];
+                            rotacaoTelaZ = resultadoEstrategia[4];
+                            translacaoTelaX = resultadoEstrategia[5];
+                            translacaoTelaY = resultadoEstrategia[6];
+                        }
+                        
+                        if ( aptidaoEnxame < aptidaoProjecaoOtima ) {
+                            aptidaoProjecaoOtima = aptidaoEnxame;
+                            escalaProjecaoX = resultadoEnxame[0];
+                            escalaProjecaoY = resultadoEnxame[1];
+                            rotacaoTelaX = resultadoEnxame[2];
+                            rotacaoTelaY = resultadoEnxame[3];
+                            rotacaoTelaZ = resultadoEnxame[4];
+                            translacaoTelaX = resultadoEnxame[5];
+                            translacaoTelaY = resultadoEnxame[6];
+                        }
+                    }
+                }
+            }
+        );
+        calibracaoParametrosProjecao.start();
+    }
+    
+    public boolean getCalibrandoParametrosProjecao() {
+        if ( calibracaoParametrosProjecao != null && calibracaoParametrosProjecao.isAlive() )
+            return true;
+        else
+            return false;
+    }
+    
+    public float[] getParametrosProjecaoOtimos() {
+        synchronized ( travaParametrosProjecao ) {
+            return new float[]{
+                escalaProjecaoX, escalaProjecaoY,
+                rotacaoTelaX, rotacaoTelaY, rotacaoTelaZ,
+                translacaoTelaX, translacaoTelaY,
+                aptidaoProjecaoOtima
+            };
+        }
+    }
+    
+    public float getEscalaProjecaoX() {
+        synchronized ( travaParametrosProjecao ) {
+            return escalaProjecaoX;
+        }
+    }
+    
+    public float getEscalaProjecaoY() {
+        synchronized ( travaParametrosProjecao ) {
+            return escalaProjecaoY;
+        }
+    }
+    
+    public float getRotacaoTelaX() {
+        synchronized ( travaParametrosProjecao ) {
+            return rotacaoTelaX;
+        }
+    }
+    
+    public float getRotacaoTelaY() {
+        synchronized ( travaParametrosProjecao ) {
+            return rotacaoTelaY;
+        }
+    }
+    
+    public float getRotacaoTelaZ() {
+        synchronized ( travaParametrosProjecao ) {
+            return rotacaoTelaZ;
+        }
+    }
+    
+    public float getTranslacaoTelaX() {
+        synchronized ( travaParametrosProjecao ) {
+            return translacaoTelaX;
+        }
+    }
+    
+    public float getTranslacaoTelaY() {
+        synchronized ( travaParametrosProjecao ) {
+            return translacaoTelaY;
+        }
+    }
+    
+    public float getAptidaoProjecaoOtima() {
+        synchronized ( travaParametrosProjecao ) {
+            return aptidaoProjecaoOtima;
+        }
+    }
+    
+    public void encerrarCalibracaoProjecao() {
+        if ( calibracaoParametrosProjecao != null )
+            calibracaoParametrosProjecao.interrupt();
+    }
+    
     @Override
     public void close() {
         encerrarCalibracaoIntrinsecos();
         encerrarEstimativa();
         encerrarCalibracaoExtrinsecos();
+        encerrarCalibracaoProjecao();
         
         if ( detectorPontos != null )
             detectorPontos.close();
